0c4e4298d93378c7946439bbdac8dbed343a4f5e
isne534 <sakatagpp@gmail.com>
2018-08-24 13:26:03 +0700

    Add foo

--- CPE200Lab1/CPE200Lab1/Form1.Designer.cs
+++ CPE200Lab1/CPE200Lab1/Form1.Designer.cs
@@ -60,6 +60,7 @@
             this.btn7.TabIndex = 0;
             this.btn7.Text = "7";
             this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btnNumber_Click);
             // 
             // btn8
             // 
@@ -70,6 +71,7 @@
             this.btn8.TabIndex = 1;
             this.btn8.Text = "8";
             this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btnNumber_Click);
             // 
             // btn9
             // 
@@ -80,6 +82,7 @@
             this.btn9.TabIndex = 2;
             this.btn9.Text = "9";
             this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btnNumber_Click);
             // 
             // btnMultiply
             // 
@@ -90,6 +93,7 @@
             this.btnMultiply.TabIndex = 3;
             this.btnMultiply.Text = "X";
             this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
             // 
             // btnMinus
             // 
@@ -100,6 +104,7 @@
             this.btnMinus.TabIndex = 7;
             this.btnMinus.Text = "-";
             this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
             // 
             // btn6
             // 
@@ -110,6 +115,7 @@
             this.btn6.TabIndex = 6;
             this.btn6.Text = "6";
             this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btnNumber_Click);
             // 
             // btn5
             // 
@@ -120,6 +126,7 @@
             this.btn5.TabIndex = 5;
             this.btn5.Text = "5";
             this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btnNumber_Click);
             // 
             // btn4
             // 
@@ -130,6 +137,7 @@
             this.btn4.TabIndex = 4;
             this.btn4.Text = "4";
             this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btnNumber_Click);
             // 
             // btnPlus
             // 
@@ -140,6 +148,7 @@
             this.btnPlus.TabIndex = 11;
             this.btnPlus.Text = "+";
             this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
             // 
             // btn3
             // 
@@ -150,6 +159,7 @@
             this.btn3.TabIndex = 10;
             this.btn3.Text = "3";
             this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btnNumber_Click);
             // 
             // btn2
             // 
@@ -160,6 +170,7 @@
             this.btn2.TabIndex = 9;
             this.btn2.Text = "2";
             this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btnNumber_Click);
             // 
             // btn1
             // 
@@ -170,6 +181,7 @@
             this.btn1.TabIndex = 8;
             this.btn1.Text = "1";
             this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btnNumber_Click);
             // 
             // btnEqual
             // 
@@ -180,6 +192,7 @@
             this.btnEqual.TabIndex = 15;
             this.btnEqual.Text = "=";
             this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
             // 
             // btnDot
             // 
@@ -190,6 +203,7 @@
             this.btnDot.TabIndex = 14;
             this.btnDot.Text = ".";
             this.btnDot.UseVisualStyleBackColor = true;
+            this.btnDot.Click += new System.EventHandler(this.btnDot_Click);
             // 
             // btn0
             // 
@@ -200,6 +214,7 @@
             this.btn0.TabIndex = 13;
             this.btn0.Text = "0";
             this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btnNumber_Click);
             // 
             // btnSign
             // 
@@ -220,6 +235,7 @@
             this.btnDivide.TabIndex = 16;
             this.btnDivide.Text = "÷";
             this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
             // 
             // btnPercent
             // 
@@ -230,6 +246,7 @@
             this.btnPercent.TabIndex = 17;
             this.btnPercent.Text = "%";
             this.btnPercent.UseVisualStyleBackColor = true;
+            this.btnPercent.Click += new System.EventHandler(this.btnPercent_Click);
             // 
             // btnClear
             // 
@@ -240,6 +257,7 @@
             this.btnClear.TabIndex = 18;
             this.btnClear.Text = "C";
             this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnBack
             // 
@@ -250,6 +268,7 @@
             this.btnBack.TabIndex = 19;
             this.btnBack.Text = "<";
             this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // lblDisplay
             // 
--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -16,5 +16,193 @@ namespace CPE200Lab1
         {
             InitializeComponent();
         }
+
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            numberBtn_Click("0");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "0";
+            check = 0;
+            first_num = 0;
+            second_num = 0;
+            hold = "";
+            look1 = 0;
+            look2 = 0;
+            sum = 0;
+            remember = 0;
+            swicth = true;
+        }
+        void numberBtn_Click(string s)
+        {
+            if (check == 1 && first_num == 0)
+            {
+                first_num = float.Parse(lblDisplay.Text);
+                check = 0;
+                lblDisplay.Text = "0";
+            }
+            if (s == "." && lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = "0.";
+            }
+            else if (lblDisplay.Text == "0" || lblDisplay.Text == "+" || lblDisplay.Text == "-" || lblDisplay.Text == "X" || lblDisplay.Text == "÷")
+            {
+                lblDisplay.Text = s;
+            }
+            else 
+            {
+                lblDisplay.Text = lblDisplay.Text + s;
+            }
+            if (first_num != 0 && !hold.Contains(".") && check == 2)
+            {
+                hold += ".";
+            }
+            if (check == 1 && hold != "")
+            {
+                second_num = float.Parse(hold);
+                if (remember == 1)
+                {
+                    second_num = first_num * second_num / 100;
+                }
+                hold = "";
+                btnOperator_Click();
+                check = 0;
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            int length =lblDisplay.Text.Length;
+            lblDisplay.Text = lblDisplay.Text.Remove(length - 1, 1);
+            check = 0;
+            look1 = 0;
+        }
+        int look1 =0;
+        int look2 = 0;
+        int check = 0;
+        string hold = "";
+        float sum = 0;
+        float first_num = 0;
+        float second_num = 0;
+        int remember = 0;
+        bool swicth = true;
+        
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            if (swicth)
+            {
+                check = 2;
+                numberBtn_Click(".");
+                swicth = false;
+            }
+        }
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            look1 = 4;
+            check = 1;
+            swicth = true;
+            numberBtn_Click("");
+            look2 = look1;
+        }
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            look1 = 1;
+            check = 1;
+            swicth = true;
+            numberBtn_Click("");
+            look2 = look1;
+        }
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            remember = 1;
+            lblDisplay.Text = lblDisplay.Text + "%";
+        }
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            look1 = 3;
+            check = 1;
+            swicth = true;
+            numberBtn_Click("");
+            look2 = look1;
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            look1 = 2;
+            check = 1;
+            swicth = true;
+            numberBtn_Click("");
+            look2 = look1;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            second_num = float.Parse(lblDisplay.Text);
+            if (remember == 1)
+            {
+                second_num = first_num * second_num / 100;
+            }
+            hold = "";
+            if (look1 == 1)
+            {
+                sum = first_num / second_num;
+            }
+            else if (look1 == 2)
+            {
+                sum = first_num * second_num;
+            }
+            else if (look1 == 3)
+            {
+                sum = first_num - second_num;
+            }
+            else if (look1 == 4)
+            {
+                sum = first_num + second_num;
+            }
+            lblDisplay.Text = sum.ToString();
+            first_num = sum;
+            sum = 0;
+            remember = 0;
+        }
+        private void btnNumber_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text == "0")
+            {
+                lblDisplay.Text = ((Button)sender).Text;
+            }
+            else if (lblDisplay.Text.Length < 8)
+            {
+                lblDisplay.Text = lblDisplay.Text + ((Button)sender).Text;
+                if (first_num != 0 && ((Button)sender).Text != "+" && ((Button)sender).Text != "-" && ((Button)sender).Text != "X" && ((Button)sender).Text != "÷" && ((Button)sender).Text != "%")
+                {
+                    hold += ((Button)sender).Text;
+                }
+            }
+        }
+        void btnOperator_Click()
+        {
+            if (look2 == 1)
+            {
+                sum = first_num / second_num;
+            }
+            else if (look2 == 2)
+            {
+                sum = first_num * second_num;
+            }
+            else if (look2 == 3)
+            {
+                sum = first_num - second_num;
+            }
+            else if (look2 == 4)
+            {
+                sum = first_num + second_num;
+            }
+            lblDisplay.Text = "0";
+            first_num = sum;
+            remember = 0;
+        }
     }
 }
